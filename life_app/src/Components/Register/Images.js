6402a9da8be35df74c53b4c8d26b57e19b98b498
anthony pegues <apegraduate8@gmail.com>
2017-06-10 15:54:26 -0400

    added config  testing

--- life_app/src/Components/Register/Images.js
+++ life_app/src/Components/Register/Images.js
@@ -1,1 +1,31 @@
+import React, { Component } from 'react';
+import ImagesUploader from 'react-images-uploader';
 
+
+
+export default class Image extends Component {
+
+
+
+  render(){
+
+         return(
+               <ImagesUploader
+                url="http://localhost:4000/api"
+                optimisticPreviews={true}
+                onLoadEnd={(err, resp) => {
+                    if(err){
+                        console.log(err, resp);
+                    }else{
+                      console.log(resp)
+                    }
+                }}
+                label="Upload multiple images"
+                inputId={this.state.formData.name}
+              />
+            )
+
+  }
+
+
+}
